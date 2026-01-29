5308326494c637b0145097d99091f450e2eff492
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-01-29 23:54:19 +0530

    upgraded functional interface code with anonymous class

--- src/Java_8_Features/Funcational_Interface.java
+++ src/Java_8_Features/Funcational_Interface.java
@@ -9,14 +9,24 @@ void show();
 }
 class Cal implements Add{
    public void show(){
-        System.out.println("in the show");
+        System.out.println("in the show, using implements and creating new class we are showing this ");
     }
 }
-
+@FunctionalInterface
+interface A{
+    void fun();
+}
 
 public class Funcational_Interface {
     public static void main (String[]args){
         Add c=new Cal();
         c.show();
+// using anonymous class
+        A a=new A(){
+            public void fun(){
+                System.out.println("Using Anonymous class we are accessing functional interact");
+            }
+        };
+        a.fun();
     }
 }
