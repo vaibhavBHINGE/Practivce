663362fc0ec9ddc7c19f342e7b1e6aa16639737f
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-01-29 23:22:46 +0530

    added for method overloading

---
+++ src/Oops/MethodOverriding.java
@@ -0,0 +1,22 @@
+package Oops;
+
+
+
+class Backend {
+    String developer(){
+        return "I know java so i am backend developer";
+    }
+}
+class FullStackDeveloper extends Backend{
+    String developer(){
+        return "I know java & react so i am FullStackDeveloper developer";
+    }
+}
+public class MethodOverriding {
+    public static void main(String[]args) {
+        FullStackDeveloper f = new FullStackDeveloper();
+        String s=f.developer();
+        System.out.println(s);
+    }
+}
+
