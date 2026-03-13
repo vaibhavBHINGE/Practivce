b4d671087be634b4aee5ebed9ac279531f22b488
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-03-13 23:24:55 +0530

    simple/singal array

---
+++ src/Arrays/SingleArray.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class SingleArray {
+    public static void main(String[]args){
+        int ar[]=new int[4];
+        ar [0]=12;
+        ar [1]=13;
+        ar [2]=14;
+        ar [3]=15;
+        for (int i=0;i<ar.length;i++){
+            System.out.println(ar[i]);
+        }
+    }
+    }
+
